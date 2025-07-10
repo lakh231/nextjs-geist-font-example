ed07860ff7fde60ccdc1b555254b75bc8b5453d6
lakh231 <t86559740@gmail.com>
2025-07-10 17:36:48 +0530

    Add bagh_chal_game/linux/flutter/ephemeral/generated_config.cmake 1752149207687

---
+++ bagh_chal_game/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/tmp/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/project/sandbox/user-workspace/bagh_chal_game" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/tmp/flutter"
+  "PROJECT_DIR=/project/sandbox/user-workspace/bagh_chal_game"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/project/sandbox/user-workspace/bagh_chal_game/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/project/sandbox/user-workspace/bagh_chal_game/lib/main.dart"
+)
